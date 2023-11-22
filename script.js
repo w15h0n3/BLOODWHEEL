8fdd00d8ac10c211806ae87be06adb33fa435c9f
w15h0n3 <151730677+w15h0n3@users.noreply.github.com>
2023-11-23 12:20:30 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -19,22 +19,56 @@ function startWheel() {
 }
 
 function getPrize(angle) {
-  if (angle >= 0 && angle < 45) {
+  if (angle >= 0 && angle < 15) {
     return 'MAJOR VICTORY';
-  } else if (angle >= 45 && angle < 90) {
+  } else if (angle >= 15 && angle < 30) {
     return 'DEATH';
-  } else if (angle >= 90 && angle < 135) {
+  } else if (angle >= 30 && angle < 45) {
+    return 'DEATH';
+  } else if (angle >= 45 && angle < 60) {
+    return 'DEATH';
+  } else if (angle >= 60 && angle < 75) {
+    return 'DEATH';
+  } else if (angle >= 75 && angle < 90) {
+    return 'DEATH';
+  } else if (angle >= 90 && angle < 105) {
+    return 'DEATH';
+  } else if (angle >= 105 && angle < 120) {
+    return 'DEATH';
+  } else if (angle >= 120 && angle < 135) {
+    return 'DEATH';
+  } else if (angle >= 135 && angle < 150) {
+    return 'DEATH';
+  } else if (angle >= 150 && angle < 165) {
+    return 'DEATH';
+  } else if (angle >= 165 && angle < 180) {
+    return 'DEATH';
+  } else if (angle >= 180 && angle < 195) {
+    return 'DEATH';
+  } else if (angle >= 195 && angle < 210) {
     return 'WISDOM IS POWER';
-  } else if (angle >= 135 && angle < 180) {
-    return 'MINOR VICTORY';
-  } else if (angle >= 180 && angle < 225) {
-    return 'STANCE REVEAL';
-  } else if (angle >= 225 && angle < 270) {
-    return 'ARTIFACT KNOWLEDGE';
-  } else if (angle >= 270 && angle < 315) {
-    return 'SUMMON REFUND';
+  } else if (angle >= 210 && angle < 225) {
+    return 'DEATH';
+  } else if (angle >= 225 && angle < 240) {
+    return 'DEATH';
+  } else if (angle >= 240 && angle < 255) {
+    return 'DEATH';
+  } else if (angle >= 255 && angle < 270) {
+    return 'DEATH';
+  } else if (angle >= 270 && angle < 285) {
+    return 'DEATH';
+  } else if (angle >= 285 && angle < 300) {
+    return 'DEATH';
+  } else if (angle >= 300 && angle < 315) {
+    return 'DEATH';
+  } else if (angle >= 315 && angle < 330) {
+    return 'DEATH';
+  } else if (angle >= 330 && angle < 345) {
+    return 'DEATH';
+  } else if (angle >= 345 && angle < 360) {
+    return 'DEATH';
   } else {
-    // Add conditions for other prizes
+    return 'UNKNOWN';
   }
 }
 
