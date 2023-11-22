faeb7124b42a7d6fcdcb2405c5e7d6f5a88ec090
w15h0n3 <151730677+w15h0n3@users.noreply.github.com>
2023-11-23 11:54:56 +1300

    Add files via upload

--- script.js
+++ script.js
@@ -19,5 +19,33 @@ function startWheel() {
 }
 
 function getPrize(angle) {
-  if (angle >= 0 && angle < 40) {
-    return
+  if (angle >= 0 && angle < 15) {
+    return 'MAJOR VICTORY';
+  } else if (angle >= 15 && angle < 30) {
+    return 'DEATH';
+  } else if (angle >= 30 && angle < 45) {
+    return 'WISDOM IS POWER';
+  } else if (angle >= 45 && angle < 60) {
+    return 'MINOR VICTORY';
+  } else if (angle >= 60 && angle < 75) {
+    return 'STANCE REVEAL';
+  } else if (angle >= 75 && angle < 90) {
+    return 'ARTIFACT KNOWLEDGE';
+  } else if (angle >= 90 && angle < 105) {
+    return 'SUMMON REFUND';
+  } else {
+    // Add conditions for other prizes
+  }
+}
+
+function displayPrize(prize) {
+  const alertDiv = document.createElement('div');
+  alertDiv.classList.add('prize-alert');
+  alertDiv.textContent = `Congratulations! You won: ${prize}`;
+
+  document.body.appendChild(alertDiv);
+
+  setTimeout(() => {
+    alertDiv.remove();
+  }, 5000);
+}
