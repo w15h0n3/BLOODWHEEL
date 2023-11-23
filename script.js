978d955b9ed29b497d7218eb7a0ba04e631ae76e
w15h0n3 <151730677+w15h0n3@users.noreply.github.com>
2023-11-23 13:03:17 +1300

    Update script.js

--- script.js
+++ script.js
@@ -7,65 +7,82 @@ var spinTimeout = null;
 var spinArcStart = 10;
 var spinTime = 0;
 var spinTimeTotal = 0;
-var current_user_status = null;
-var spin_results = null;
 var ctx;
 
 function drawSpinnerWheel() {
-  var canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    var outsideRadius = 200;
-    var textRadius = 160;
-    var insideRadius = 125;
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+        var outsideRadius = 200;
+        var textRadius = 160;
+        var insideRadius = 125;
 
-    ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, 500, 500);
+        ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, 500, 500);
 
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
+        ctx.lineWidth = 2;
 
-    ctx.font = 'bold 12px Helvetica, Arial';
+        ctx.font = 'bold 12px Helvetica, Arial';
 
-    for (var i = 0; i < 8; i++) {
-      var angle = startAngle + i * arc;
-      ctx.fillStyle = colors[i];
+        for (var i = 0; i < 8; i++) {
+            var angle = startAngle + i * arc;
 
-      ctx.beginPath();
-      ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
-      ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
-      ctx.stroke();
-      ctx.fill();
+            if (prize_descriptions[i] === "DEATH") {
+                // If it's a death segment, fill with black and add a white border
+                ctx.fillStyle = "black";
+                ctx.strokeStyle = "white";
+            } else {
+                // For other segments, use random colors
+                ctx.fillStyle = getRandomColor();
+                ctx.strokeStyle = "black";
+            }
 
-      ctx.save();
-      ctx.shadowOffsetX = -1;
-      ctx.shadowOffsetY = -1;
-      ctx.shadowBlur = 0;
-      ctx.shadowColor = "rgb(220,220,220)";
-      ctx.fillStyle = "white";
-      ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius,
-        250 + Math.sin(angle + arc / 2) * textRadius);
-      ctx.rotate(angle + arc / 2 + Math.PI / 2);
-      var text = prize_descriptions[i];
-      if (text == undefined) text = "Not this time! " + i;
-      ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-      ctx.restore();
+            ctx.beginPath();
+            ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
+            ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
+            ctx.stroke();
+            ctx.fill();
+
+            ctx.save();
+            ctx.shadowOffsetX = -1;
+            ctx.shadowOffsetY = -1;
+            ctx.shadowBlur = 0;
+            ctx.shadowColor = "rgb(220,220,220)";
+            ctx.fillStyle = "white";
+            ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius,
+                250 + Math.sin(angle + arc / 2) * textRadius);
+            ctx.rotate(angle + arc / 2 + Math.PI / 2);
+            var text = prize_descriptions[i];
+            if (text == undefined) text = "Not this time! " + i;
+            ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+            ctx.restore();
+        }
+
+        //Arrow
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
+        ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
+        ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
+        ctx.fill();
     }
-
-    //Arrow
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
-    ctx.fill();
-  }
 }
 
-function startSpinning() {
-  // Implement the spinning logic here
-  // For now, let's just redraw the wheel
-  drawSpinnerWheel();
+function getRandomColor() {
+    // Generate a random color in hex format
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
-// Call the drawSpinnerWheel function when the page loads
-window.onload = drawSpinnerWheel;
+function spin() {
+    spinAngleStart = Math.random() * 10 + 10;
+    spinTime = 0;
+    spinTimeTotal = Math.random() * 3 + 4 * 1000;
+    rotateWheel();
+}
+
+function rotateWheel() {
+    spinTime += 30;
+    if (spinTime >= spinTimeTotal) {
+        stopRotateWheel();
+        return;
+    }
+    var spinAngle = spinAngleStart - easeOut
