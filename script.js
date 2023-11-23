8e69e4c99824d90f47d6c39a1ecb9cc8e09951a7
w15h0n3 <151730677+w15h0n3@users.noreply.github.com>
2023-11-23 13:25:52 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-var colors = ["#4d0000", "#4d0000", "#4d0000", "#4d0000", "#4d0000", "#4d0000", "#4d0000", "#4d0000"];
-var prizeDescriptions = ["MAJOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "WISDOM IS POWER", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "MINOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "WISDOM IS POWER", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "STANCE REVEAL", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "MINOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "ARTIFACT KNOWLEDGE", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "MINOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "SUMMON REFUND", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "MINOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH"];
+document.addEventListener('DOMContentLoaded', function () {
+  var colors = ["#4d0000", "#4d0000", "#4d0000", "#4d0000", "#4d0000", "#4d0000", "#4d0000", "#4d0000"];
+  var prizeDescriptions = ["MAJOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "WISDOM IS POWER", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "MINOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "WISDOM IS POWER", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "STANCE REVEAL", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "MINOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "ARTIFACT KNOWLEDGE", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "MINOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "SUMMON REFUND", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "MINOR VICTORY", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH"];
 
-var startAngle = 0;
-var arc = Math.PI / 4;
-var spinTimeout = null;
-var spinArcStart = 10;
-var spinTime = 0;
-var spinTimeTotal = 0;
-var ctx;
+  var startAngle = 0;
+  var arc = Math.PI / 4;
+  var spinTimeout = null;
+  var spinArcStart = 10;
+  var spinTime = 0;
+  var spinTimeTotal = 0;
+  var ctx;
 
-function drawSpinnerWheel() {
-  var canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    var outsideRadius = 200;
-    var textRadius = 160;
-    var insideRadius = 125;
+  function drawSpinnerWheel() {
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      var outsideRadius = 200;
+      var textRadius = 160;
+      var insideRadius = 125;
 
-    ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, 500, 
+      ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, 500, 500);
+
+      ctx.strokeStyle = "#ffffff"; // White outline for each segment
+      ctx.lineWidth = 2;
+
+      for (var i = 0; i < 8; i++) {
+        var angle = startAngle + i * arc;
+        ctx.fillStyle = colors[i];
+
+        ctx.beginPath();
+        ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
+        ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
+        ctx.stroke();
+        ctx.fill();
+
+        ctx.save();
+        ctx.shadowOffsetX = -1;
+        ctx.shadowOffsetY = -1;
+        ctx.shadowBlur = 0;
+        ctx.shadowColor = "rgb(220,220,220)";
+        ctx.fillStyle = "white";
+        ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius,
+          250 + Math.sin(angle + arc / 2) * textRadius);
+        ctx.rotate(angle + arc / 2 + Math.PI / 2);
+        var text = prizeDescriptions[i];
+        if (text == undefined) text = "Not this time! " + i;
+        ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+        ctx.restore();
+      }
+
+      // Arrow
+      ctx.fillStyle = "white";
+      ctx.beginPath();
+      ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
+      ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
+      ctx.lineTo(250 + 4
