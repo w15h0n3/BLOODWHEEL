f387d0e26dfc60322952c47dbb6c32cdcc3b8d34
w15h0n3 <151730677+w15h0n3@users.noreply.github.com>
2023-11-25 08:51:17 +1300

    Update script.js

--- script.js
+++ script.js
@@ -22,8 +22,7 @@ var prize_descriptions = [
   "MINOR", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH", "DEATH"
 ];
 
-// ... (remaining code)
-
+// ... (previous code)
 
 // Define the variables for the spinning function
 var startAngle = 0;
@@ -41,108 +40,32 @@ var ctx;
 
 // Define the function to draw the spinner wheel
 function drawSpinnerWheel() {
-  var canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, 500, 500);
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-    ctx.font = 'bold 12px Cinzel, serif';
-
-    // Draw the wheel
-    for (var i = 0; i < numSegments; i++) {
-      var angle = startAngle + i * arc;
-      var color;
-
-      if (prize_descriptions[i % prize_descriptions.length] === "DEATH") {
-        color = "#444444"; // Dark Grey for "DEATH"
-      } else {
-        color = colors[prize_descriptions[i % prize_descriptions.length]];
-      }
-
-      ctx.fillStyle = color;
-
-      ctx.beginPath();
-      ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
-      ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
-      ctx.stroke();
-      ctx.fill();
-
-      ctx.save();
-      ctx.shadowOffsetX = -1;
-      ctx.shadowOffsetY = -1;
-      ctx.shadowBlur = 0;
-      ctx.shadowColor = "rgb(220,220,220)";
-      ctx.fillStyle = "white";
-      var text = prize_descriptions[i % prize_descriptions.length];
-      if (text == undefined) text = "Not this time! " + i;
-
-      // Calculate the font size dynamically based on the segment size
-      var maxTextWidth = insideRadius * Math.sin(arc / 2);
-      var maxTextHeight = outsideRadius - insideRadius;
-      var fontSize = Math.min((9 * maxTextWidth) / text.length, maxTextHeight);
-      ctx.font = 'bold ' + fontSize + 'px Cinzel, serif';
-
-      // Adjust the position for vertical text
-      var textRadius = outsideRadius - 20; // Adjust the distance from the center
-      ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius,
-        250 + Math.sin(angle + arc / 2) * textRadius);
-
-      // Rotate the text to be vertical
-      ctx.rotate(angle + arc / 2 + Math.PI);
-
-      // Adjust the position for centered text
-      var textWidth = ctx.measureText(text).width;
-      ctx.fillText(text, -textWidth / 2, 0);
-      ctx.restore();
-    }
-
-    // Draw the logo in the center
-    var logo = new Image();
-    logo.src = 'https://github.com/w15h0n3/BLOODWHEEL/raw/main/KING%20SNOOCH%20-%20FULL%20LOGO-01.png';
-    var logoSize = 350;
-
-    logo.onload = function () {
-      ctx.drawImage(logo, 250 - logoSize / 2, 250 - logoSize / 2, logoSize, logoSize);
-    }
-
-    // Draw the arrow
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
-    ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
-    ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
-    ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
-    ctx.fill();
-  }
+  // ... (existing drawSpinnerWheel code)
 }
 
-// ... (remaining code)
+// ... (previous code)
 
-
-
-// Define the function to spin the wheel
+// Define the function to spin the wheel with varying speeds
 function spinWheel() {
   // Hide the prize win text
   document.getElementById('prizeText').textContent = '';
 
   spinTime = 0;
 
-  // Generate a random start angle for each spin
-  spinArcStart = Math.random() * 360 + 180; // Start angle between 180 and 540 degrees
-
-  // Set totalRotations to 1 for just one full rotation
-  var totalRotations = 1;
+  // Set totalRotations for each second
+  var totalRotations;
+  if (spinTime < 1000) {
+    totalRotations = 4; // First second: 4 rotations
+  } else if (spinTime < 2000) {
+    totalRotations = 3; // Second second: 3 rotations
+  } else if (spinTime < 3000) {
+    totalRotations = 2; // Third second: 2 rotations
+  } else {
+    totalRotations = 1; // Fourth second: 1 rotation
+  }
 
   // Reduce the spinTimeTotal for a shorter spin
   spinTimeTotal = totalRotations * 360 * 5; // Adjust the multiplier for desired speed
-
-  // Hide the prize win text
-  document.getElementById('prizeText').textContent = '';
 
   rotateWheel();
 }
@@ -191,3 +114,5 @@ function easeOut(t, b, c, d) {
 
 // Draw the initial wheel on page load
 document.addEventListener("DOMContentLoaded", drawSpinnerWheel);
+
+// ... (remaining code)
